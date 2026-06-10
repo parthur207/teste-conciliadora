33d265602f8bbcb232f02cf424d9eaae77818e7f
Paulo Andrade <parthur207@gmail.com>
2026-06-10 18:47:13 -0300

    Criação de um DTO que coleta as colunas do csv para manipulação.

---
+++ src/backend/Dtos/VeiculoClienteCsvDtos.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+public class VeiculoCsvDto
+{
+    [Name("placa")]
+    public string Placa { get; set; } = string.Empty;
+
+    [Name("modelo")]
+    public string Modelo { get; set; } = string.Empty;
+
+    [Name("ano")]
+    public string Ano { get; set; } = string.Empty;
+
+    [Name("cliente_nome")]
+    public string ClienteNome { get; set; } = string.Empty;
+
+    [Name("cliente_telefone")]
+    public string ClienteTelefone { get; set; } = string.Empty;
+
+    [Name("cliente_endereco")]
+    public string ClienteEndereco { get; set; } = string.Empty;
+
+    [Name("mensalista")]
+    public string Mensalista { get; set; } = string.Empty;
+
+    [Name("valor_mensalidade")]
+    public string ValorMensalidade { get; set; } = string.Empty;
+}
