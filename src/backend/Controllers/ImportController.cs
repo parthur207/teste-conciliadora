244bfae1845c3c2d8ffa0c537e6c4fe3e66138a0
Paulo Andrade <parthur207@gmail.com>
2026-06-10 19:21:26 -0300

    Correção.

--- src/backend/Controllers/ImportController.cs
+++ src/backend/Controllers/ImportController.cs
@@ -1,25 +1,8 @@
-
-using CsvHelper;
-using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Parking.Api.Data;
 using Parking.Api.Interfaces;
-using Parking.Api.Models;
-using Parking.Api.Services;
-using System.Globalization;
-using System.Text;
 
 namespace Parking.Api.Controllers
 {
-    [ApiController]
-    [Route("api/import")]
-    public class ImportController : ControllerBase
-    {
-        private readonly AppDbContext _db;
-        private readonly PlacaService _placa;
-        public ImportController(AppDbContext db, PlacaService placa) { _db = db; _placa = placa; }
-
         [ApiController]
         [Route("api/import")]
         public class ImportController : ControllerBase
